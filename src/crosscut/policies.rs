0e31bf8f1ca706bc82d082b34be96d456c4fb8b5
Santiago Carmuega <santiago@carmuega.me>
2022-06-25 19:34:30 -0300

    feat: Implement pipeline-wide policy config

--- src/crosscut/policies.rs
+++ src/crosscut/policies.rs
@@ -1,56 +1,63 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Serialize, Deserialize)]
-pub struct ReducerPolicy {
-    skip_missing_data: Option<bool>,
-    skip_cbor_errors: Option<bool>,
-    skip_ledger_errors: Option<bool>,
-    skip_all_errors: Option<bool>,
+use crate::Error;
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, PartialOrd)]
+pub enum ErrorAction {
+    Skip,
+    Warn,
+    Default,
+}
+
+impl Default for ErrorAction {
+    fn default() -> Self {
+        ErrorAction::Default
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, Default)]
+pub struct RuntimePolicy {
+    missing_data: Option<ErrorAction>,
+    cbor_errors: Option<ErrorAction>,
+    ledger_errors: Option<ErrorAction>,
+    any_error: Option<ErrorAction>,
+}
+
+#[inline]
+fn handle_error<T>(err: Error, action: &Option<ErrorAction>) -> Result<Option<T>, crate::Error> {
+    match action {
+        Some(ErrorAction::Skip) => Ok(None),
+        Some(ErrorAction::Warn) => {
+            log::warn!("{}", err);
+            Ok(None)
+        }
+        _ => Err(err),
+    }
 }
 
 pub trait AppliesPolicy {
     type Value;
 
-    fn apply_policy(
-        self,
-        policy: &Option<ReducerPolicy>,
-    ) -> Result<Option<Self::Value>, crate::Error>;
+    fn apply_policy(self, policy: &RuntimePolicy) -> Result<Option<Self::Value>, crate::Error>;
 }
 
 impl<T> AppliesPolicy for Result<T, crate::Error> {
     type Value = T;
 
-    fn apply_policy(
-        self,
-        policy: &Option<ReducerPolicy>,
-    ) -> Result<Option<Self::Value>, crate::Error> {
+    fn apply_policy(self, policy: &RuntimePolicy) -> Result<Option<Self::Value>, crate::Error> {
         match self {
             Ok(t) => Ok(Some(t)),
             Err(err) => {
-
-                if matches!(policy, None) {
-                    return Err(err);
+                // apply generic error policy if we have one
+                if matches!(policy.any_error, Some(_)) {
+                    return handle_error(err, &policy.any_error);
                 }
 
-                let policy = policy.as_ref().unwrap();
-
-                if policy.skip_all_errors.unwrap_or(false) {
-                    return Ok(None);
-                }
-
+                // apply specific actions for each type of error
                 match &err {
-                    crate::Error::MissingTx(_) if policy.skip_missing_data.unwrap_or(false) => {
-                        log::warn!("missing tx:{}", err);
-                        Ok(None)
-                    }
-                    crate::Error::CborError(_) if policy.skip_cbor_errors.unwrap_or(false) => {
-                        log::warn!("cbor error:{}", err);
-                        Ok(None)
-                    }
-                    crate::Error::LedgerError(_) if policy.skip_ledger_errors.unwrap_or(false) => {
-                        log::warn!("ledger error:{}", err);
-                        Ok(None)
-                    }
+                    crate::Error::MissingTx(_) => handle_error(err, &policy.missing_data),
+                    crate::Error::CborError(_) => handle_error(err, &policy.cbor_errors),
+                    crate::Error::LedgerError(_) => handle_error(err, &policy.ledger_errors),
                     _ => Err(err),
                 }
             }
