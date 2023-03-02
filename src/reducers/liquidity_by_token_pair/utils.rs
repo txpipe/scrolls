c51fd3e7ce50082d9b42aa39cebf08b4b3864c42
William Wolff <william.wolff@iohk.io>
2023-03-02 23:33:21 +0000

    feat: add helper method to generate redis key for token pair

--- src/reducers/liquidity_by_token_pair/utils.rs
+++ src/reducers/liquidity_by_token_pair/utils.rs
@@ -3,7 +3,7 @@ use pallas::ledger::{
     traverse::Asset,
 };
 
-use super::model::PoolAsset;
+use super::model::{PoolAsset, TokenPair};
 
 pub fn policy_id_from_str(str: &Vec<u8>) -> PolicyId {
     let mut pid: [u8; 28] = [0; 28];
@@ -28,19 +28,6 @@ pub fn contains_currency_symbol(currency_symbol: &String, assets: &Vec<Asset>) -
     })
 }
 
-pub fn filter_by_native_fungible(assets: Vec<Asset>) -> Vec<Asset> {
-    let mut result: Vec<Asset> = Vec::new();
-    for asset in assets {
-        match asset {
-            Asset::NativeAsset(_, _, quantity) if quantity > 1 => {
-                result.push(asset);
-            }
-            _ => (),
-        }
-    }
-    result
-}
-
 pub fn pool_asset_from(hex_currency_symbol: &String, hex_asset_name: &String) -> Option<PoolAsset> {
     if hex_currency_symbol.len() == 0 && hex_asset_name.len() == 0 {
         return Some(PoolAsset::Ada);
@@ -59,15 +46,54 @@ pub fn pool_asset_from(hex_currency_symbol: &String, hex_asset_name: &String) ->
     None
 }
 
+pub fn key(token_pair: TokenPair) -> Option<String> {
+    match (token_pair.coin_a, token_pair.coin_b) {
+        (PoolAsset::Ada, PoolAsset::AssetClass(cs, tkn))
+        | (PoolAsset::AssetClass(cs, tkn), PoolAsset::Ada) => Some(format!(
+            "{}.{}",
+            hex::encode(cs.to_vec()),
+            hex::encode(tkn.to_vec())
+        )),
+        (PoolAsset::AssetClass(cs1, tkn1), PoolAsset::AssetClass(cs2, tkn2)) => {
+            let asset_id_1 = format!(
+                "{}.{}",
+                hex::encode(cs1.to_vec()),
+                hex::encode(tkn1.to_vec())
+            );
+            let asset_id_2 = format!(
+                "{}.{}",
+                hex::encode(cs2.to_vec()),
+                hex::encode(tkn2.to_vec())
+            );
+
+            match asset_id_1.cmp(&asset_id_2) {
+                std::cmp::Ordering::Less => Some(format!("{}:{}", asset_id_1, asset_id_2,)),
+                std::cmp::Ordering::Greater => Some(format!("{}:{}", asset_id_2, asset_id_1,)),
+                _ => None,
+            }
+        }
+        _ => None,
+    }
+}
+
 #[cfg(test)]
 mod test {
     use std::str::FromStr;
 
-    use pallas::ledger::{primitives::babbage::PolicyId, traverse::Asset};
-
-    use crate::reducers::liquidity_by_token_pair::utils::{
-        contains_currency_symbol, filter_by_native_fungible,
+    use pallas::ledger::{
+        primitives::{
+            babbage::{PlutusData, PolicyId},
+            Fragment,
+        },
+        traverse::Asset,
     };
+
+    use crate::reducers::liquidity_by_token_pair::{
+        model::{PoolAsset, TokenPair},
+        utils::contains_currency_symbol,
+    };
+
+    use super::key;
 
     static CURRENCY_SYMBOL_1: &str = "93744265ed9762d8fa52c4aacacc703aa8c81de9f6d1a59f2299235b";
     static CURRENCY_SYMBOL_2: &str = "158fd94afa7ee07055ccdee0ba68637fe0e700d0e58e8d12eca5be46";
@@ -111,12 +137,79 @@ mod test {
     }
 
     #[test]
-    fn test_filter_by_native_fungible() {
-        let filtered = filter_by_native_fungible(mock_assets());
-        assert_eq!(filtered.len(), 2);
+    fn test_valid_key_for_ada_min() {
+        let hex_pool_datum = "d8799fd8799f4040ffd8799f581c29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6434d494eff1b00004ce6fb73282200d87a80ff";
+        let data = hex::decode(hex_pool_datum).unwrap();
+        let plutus_data = PlutusData::decode_fragment(&data).unwrap();
+        let token_pair = TokenPair::try_from(&plutus_data).unwrap();
+        let key = key(token_pair);
+        assert_eq!(true, key.is_some());
         assert_eq!(
-            filtered.get(0).unwrap().ascii_name(),
-            Some(String::from("Tkn2"))
-        );
+            "29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e",
+            key.unwrap()
+        )
+    }
+
+    #[test]
+    fn test_valid_key_for_min_ada() {
+        let hex_pool_datum = "d8799fd8799f4040ffd8799f581c29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6434d494eff1b00004ce6fb73282200d87a80ff";
+        let data = hex::decode(hex_pool_datum).unwrap();
+        let plutus_data = PlutusData::decode_fragment(&data).unwrap();
+        let tp = TokenPair::try_from(&plutus_data).unwrap();
+        let token_pair = TokenPair {
+            coin_a: tp.coin_b,
+            coin_b: tp.coin_a,
+        };
+        let key = key(token_pair);
+        assert_eq!(true, key.is_some());
+        assert_eq!(
+            "29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e",
+            key.unwrap()
+        )
+    }
+
+    #[test]
+    fn test_valid_key_for_same_assets() {
+        let hex_pool_datum = "d8799fd8799f4040ffd8799f581c29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6434d494eff1b00004ce6fb73282200d87a80ff";
+        let data = hex::decode(hex_pool_datum).unwrap();
+        let plutus_data = PlutusData::decode_fragment(&data).unwrap();
+        let tp1 = TokenPair::try_from(&plutus_data).unwrap();
+        let tp2 = TokenPair::try_from(&plutus_data).unwrap();
+
+        let tp1_invalid = TokenPair {
+            coin_a: tp1.coin_b,
+            coin_b: tp2.coin_b,
+        };
+        let key1 = key(tp1_invalid);
+        assert_eq!(true, key1.is_none());
+
+        let tp2_invalid = TokenPair {
+            coin_a: tp1.coin_a,
+            coin_b: tp2.coin_a,
+        };
+        let key2 = key(tp2_invalid);
+        assert_eq!(true, key2.is_none());
+    }
+
+    #[test]
+    fn test_valid_key_for_min_djed() {
+        let hex_pool_datum = "d8799fd8799f581c29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6434d494effd8799f581c8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd614c446a65644d6963726f555344ff1b000000012d9b96321b000000012dc40542d8799fd8799fd8799fd8799f581caafb1196434cb837fd6f21323ca37b302dff6387e8a84b3fa28faf56ffd8799fd8799fd8799f581c52563c5410bff6a0d43ccebb7c37e1f69f5eb260552521adff33b9c2ffffffffd87a80ffffff";
+        let data = hex::decode(hex_pool_datum).unwrap();
+        let plutus_data = PlutusData::decode_fragment(&data).unwrap();
+        let token_pair = TokenPair::try_from(&plutus_data).unwrap();
+
+        let key = key(token_pair);
+        assert_eq!(true, key.is_some());
+        assert_eq!("29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6.4d494e:8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61.446a65644d6963726f555344", key.unwrap());
+    }
+
+    #[test]
+    fn test_invalid_key_for_ada_ada() {
+        let token_pair = TokenPair {
+            coin_a: PoolAsset::Ada,
+            coin_b: PoolAsset::Ada,
+        };
+        let key = key(token_pair);
+        assert_eq!(true, key.is_none());
     }
 }
