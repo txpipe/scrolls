a5663dd00bd53f4718937eade935aadd49f2d815
William Wolff <william.wolff@iohk.io>
2023-03-03 03:10:40 +0000

    feat: complete reducer implementation for retrieving key/ value pair & producing redis crdt commands

--- src/reducers/liquidity_by_token_pair/mod.rs
+++ src/reducers/liquidity_by_token_pair/mod.rs
@@ -1,4 +1,11 @@
-use pallas::ledger::traverse::{MultiEraBlock, MultiEraOutput, MultiEraTx, OutputRef};
+use lazy_static::__Deref;
+use pallas::{
+    codec::utils::CborWrap,
+    ledger::{
+        primitives::babbage::DatumOption,
+        traverse::{Asset, MultiEraBlock, MultiEraOutput},
+    },
+};
 use serde::Deserialize;
 
 pub mod minswap;
@@ -9,7 +16,12 @@ pub mod wingriders;
 
 use crate::{crosscut, prelude::*};
 
-use self::utils::contains_currency_symbol;
+use self::{
+    model::{PoolAsset, PoolDatum, TokenPair},
+    sundaeswap::SundaePoolDatum,
+    utils::contains_currency_symbol,
+    wingriders::WingriderPoolDatum,
+};
 
 #[derive(Deserialize)]
 pub struct Config {
@@ -23,42 +35,76 @@ pub struct Reducer {
     policy: crosscut::policies::RuntimePolicy,
 }
 
-impl Reducer {
-    fn process_consumed_txo(
-        &mut self,
-        ctx: &crate::model::BlockContext,
-        input: &OutputRef,
-        output: &mut super::OutputPort,
-    ) -> Result<(), gasket::error::Error> {
-        let utxo = ctx.find_utxo(input).apply_policy(&self.policy).or_panic()?;
-        // Skip invalid or byron era outputs
-        let utxo = match utxo {
-            None => return Ok(()),
-            Some(MultiEraOutput::Byron(_)) => return Ok(()),
-            Some(x) => x,
-        };
+fn get_asset_amount(asset: &PoolAsset, assets: &Vec<Asset>) -> Option<u64> {
+    match asset {
+        PoolAsset::Ada => {
+            for a in assets {
+                if let Asset::Ada(amt) = a {
+                    return Some(*amt);
+                }
+            }
+        }
+        PoolAsset::AssetClass(ppid, ptkn) => {
+            let pid_str = hex::encode(ppid.deref().to_vec());
+            let tkn_str = hex::encode(ptkn.deref());
+            for a in assets {
+                if let Asset::NativeAsset(pid, tkn, amt) = a {
+                    if hex::encode(pid.deref()).eq(&pid_str) && hex::encode(tkn).eq(&tkn_str) {
+                        return Some(*amt);
+                    }
+                }
+            }
+        }
+    }
 
-        // Skip any spending transaction that has no currency symbol of an identifiable liquidity source
+    None
+}
+
+impl Reducer {
+    fn get_key_value_pair(&self, utxo: &MultiEraOutput) -> Result<(String, String), ()> {
         if !contains_currency_symbol(
             &self.config.pool_currency_symbol,
             utxo.non_ada_assets().as_ref(),
         ) {
-            return Ok(());
+            return Err(());
         }
-        // let fungible_non_ada_assets = filter_by_native_fungible(utxo.non_ada_assets());
 
-        Ok(())
-    }
+        // Try to get embedded datum & decode datum data to supported pool datum
+        if let Some(DatumOption::Data(CborWrap(pd))) = utxo.datum() {
+            if let Some(pool_datum) = PoolDatum::try_from(&pd).ok() {
+                let assets = utxo.assets();
+                return match pool_datum {
+                    PoolDatum::Minswap(TokenPair { coin_a, coin_b })
+                    | PoolDatum::Wingriders(WingriderPoolDatum { coin_a, coin_b }) => {
+                        if let (Some(coin_a_amt), Some(coin_b_amt), Some(key)) = (
+                            get_asset_amount(&coin_a, &assets),
+                            get_asset_amount(&coin_b, &assets),
+                            TokenPair { coin_a, coin_b }.key(),
+                        ) {
+                            return Ok((key, format!("{}:{}", coin_a_amt, coin_b_amt)));
+                        }
 
-    fn process_produced_txo(
-        &mut self,
-        tx: &MultiEraTx,
-        tx_output: &MultiEraOutput,
-        output_idx: usize,
-        output: &mut super::OutputPort,
-    ) -> Result<(), gasket::error::Error> {
-        // ToDo: Implementation
-        Ok(())
+                        Err(())
+                    }
+                    PoolDatum::Sundaeswap(SundaePoolDatum {
+                        coin_a,
+                        coin_b,
+                        fee,
+                    }) => {
+                        if let (Some(coin_a_amt), Some(coin_b_amt), Some(key)) = (
+                            get_asset_amount(&coin_a, &assets),
+                            get_asset_amount(&coin_b, &assets),
+                            TokenPair { coin_a, coin_b }.key(),
+                        ) {
+                            return Ok((key, format!("{}:{}:{}", coin_a_amt, coin_b_amt, fee)));
+                        }
+
+                        Err(())
+                    }
+                };
+            }
+        }
+        Err(())
     }
 
     pub fn reduce_block<'b>(
@@ -67,13 +113,26 @@ impl Reducer {
         ctx: &crate::model::BlockContext,
         output: &mut super::OutputPort,
     ) -> Result<(), gasket::error::Error> {
+        let pool_prefix = self.config.pool_prefix.as_deref();
         for tx in block.txs().into_iter() {
             for consumed in tx.consumes().iter().map(|i| i.output_ref()) {
-                self.process_consumed_txo(&ctx, &consumed, output)?;
+                if let Some(utxo) = ctx
+                    .find_utxo(&consumed)
+                    .apply_policy(&self.policy)
+                    .or_panic()?
+                {
+                    if let Some((k, v)) = self.get_key_value_pair(&utxo).ok() {
+                        output.send(
+                            crate::model::CRDTCommand::set_remove(pool_prefix, &k, v).into(),
+                        )?;
+                    }
+                }
             }
 
-            for (idx, produced) in tx.produces() {
-                self.process_produced_txo(&tx, &produced, idx, output)?;
+            for (_, produced) in tx.produces() {
+                if let Some((k, v)) = self.get_key_value_pair(&produced).ok() {
+                    output.send(crate::model::CRDTCommand::set_add(pool_prefix, &k, v).into())?;
+                }
             }
         }
 
